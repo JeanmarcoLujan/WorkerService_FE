a5c87d110fb0e651f540693caf79e4c5741440a5
JeanmarcoLujan <mark.15@hotmail.es>
2024-01-24 13:11:42 -0500

    Add project files.

---
+++ WorkerService_FE/Program.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using WorkerService_FE;
+
+//IHost host = Host.CreateDefaultBuilder(args)
+//    .ConfigureServices(services =>
+//    {
+//        services.AddHostedService<Worker>();
+//    })
+//    .Build();
+
+//await host.RunAsync();
+
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        // Configure the Windows Service Name.
+        options.ServiceName = "NXAFIPM.Demo";
+    })
+    .ConfigureServices(services =>
+    {
+        // Register the primary worker service.
+        services.AddHostedService<Worker>();
+        //services.AddSingleton<IServiceLayerRepository, ServiceLayerRepository>();
+
+        //services.AddSingleton<ISAPRepository, SAPRepository>(); //ISlRepository
+        //services.AddSingleton<ISlRepository, SlRepository>();
+        //services.AddSingleton<IAfipRepository, AfipRepository>();
+        //services.AddSingleton<ILoggingRepository, LoggingRepository>();
+
+        //services.AddHttpClient();
+
+        //var configuration = new ConfigurationBuilder()
+        //.SetBasePath(Directory.GetCurrentDirectory())
+        //.AddJsonFile("appsettings.json")
+        //.Build();
+
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+        .AddJsonFile("appsettings.json");
+
+
+
+        services.AddSingleton(configuration);
+
+        // Register other services here...
+    })
+    .Build();
+
+// Run the application.
+await host.RunAsync();
---
+++ WorkerService_FE_Request/Repository/RequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerService_FE_Request.Repository
+{
+    public class RequestRepository
+    {
+    }
+}
